583ff08772ab8caacf62b2a087771f771e7c1699
alithegamer565 <alithegamer565@gmail.com>
2025-10-31 04:03:09 +0500

    Delete tools-with-fun directory

--- tools-with-fun/pages/api/tools.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs'
-import path from 'path'
-export default function handler(req,res){
-  const p = path.join(process.cwd(),'data','tools.json')
-  const tools = JSON.parse(fs.readFileSync(p,'utf8'))
-  res.status(200).json({tools})
-}
